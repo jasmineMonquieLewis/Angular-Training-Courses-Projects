f8a83db8c5298b0b6b640a779935385dac7328e3
Jasmine Monquie Lewis <jasminemonquie.L@gmail.com>
2019-08-22 16:03:34 -0500

    Add Product & Shared Modules and Routing
    Decompose routing in App Module into a product-routing-module and created a product-module and shared-module

--- Angular Getting Started/APM/src/app/app.module.ts
+++ Angular Getting Started/APM/src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './app-routing.module';
+import { ProductRoutingModule } from './products/product-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    HttpClientModule,
+    ProductModule,
+    ProductRoutingModule,
+    AppRoutingModule
   ],
   bootstrap: [AppComponent]
 })
--- Angular Getting Started/APM/src/app/products/product-detail.component.ts
+++ Angular Getting Started/APM/src/app/products/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -12,27 +13,27 @@ import { IProduct } from './product';
 //in another component
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
-  product: IProduct;
+  product: IProduct | undefined;
   imageWidth: number = 200;
   imageMargin: number = 2;
 
   constructor(private route: ActivatedRoute,
-                private router: Router) { }
+                private router: Router,
+                private productService: ProductService) { }
 
   ngOnInit() {
-    //add a '+' at the beginning bcuz the parameter 'id' is a string need to convert to a number
-    let id = +this.route.snapshot.paramMap.get('id');
-    this.pageTitle += `: ${id}`; //using back ticks to display the template
-    this.product = {
-      "productId": id,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2019",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
+    let param = this.route.snapshot.paramMap.get('id');
+    if (param) {
+       //add a '+' at the beginning bcuz the parameter 'id' is a string need to convert to a number
+      const id = +param;
+      this.getProduct(id);
     }
+  }
+
+  getProduct(id: number) {
+    this.productService.getProuct(id).subscribe({
+      next: product => this.product = product
+    });
   }
 
   onBack(): void {
