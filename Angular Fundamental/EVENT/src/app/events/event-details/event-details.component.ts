9cf5954870de8702c4224e42885057505a34a14f
Jasmine Monquie Lewis <jasminemonquie.L@gmail.com>
2019-10-30 12:59:28 -0500

    Addedd Filter & Sort by Event Sessions
    Created a custome sort and filter

--- Angular Fundamental/EVENT/src/app/events/event-details/event-details.component.ts
+++ Angular Fundamental/EVENT/src/app/events/event-details/event-details.component.ts
@@ -8,13 +8,25 @@ import { IEvent, ISession } from '../shared/index';
   styleUrls: ['./event-details.component.css']
 })
 export class EventDetailsComponent implements OnInit {
-  public event: IEvent;
   public addMode: boolean;
+  public event: IEvent;
+  public filterBy: string = 'all';
+  public sortBy: string = 'votes';
+
+  //filters
+  public allFilter: string = 'all';
+  public beginnerFilter: string = 'beginner';
+  public intermediateFilter: string = 'intermediate';
+  public advancedFilter: string = 'advanced';
+
+  //sorts
+  public nameSort: string = 'name';
+  public voteSort: string = 'votes';
 
   constructor(
     @Inject(EventService) private eventService: EventService,
     @Inject(ActivatedRoute) private route: ActivatedRoute
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
@@ -22,6 +34,10 @@ export class EventDetailsComponent implements OnInit {
 
   public addSession(): void {
     this.addMode = true;
+  }
+
+  public cancelAddSession(): void {
+    this.addMode = false;
   }
 
   public saveNewSession(session: ISession): void {
@@ -33,10 +49,6 @@ export class EventDetailsComponent implements OnInit {
 
     this.event.sessions.push(session);
     this.eventService.updateEvent(this.event);
-    this.addMode = false;
-  }
-
-  public cancelAddSession(): void {
     this.addMode = false;
   }
 }
--- Angular Fundamental/EVENT/src/app/events/event-details/session-list.component.ts
+++ Angular Fundamental/EVENT/src/app/events/event-details/session-list.component.ts
@@ -1,17 +1,53 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared/index';
+import { retry } from 'rxjs/operators';
 
 @Component({
   selector: 'session-list',
   templateUrl: './session-list.component.html',
   styleUrls: ['./session-list.component.css']
 })
-export class SessionListComponent implements OnInit {
+export class SessionListComponent implements OnChanges {
   @Input() sessions: ISession[];
+  @Input() filterBy: string;
+  @Input() sortBy: string;
 
-  constructor() { }
+  visibleSessions: ISession[] = [];
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnChanges(): void {
+    //check to see if sessions is set
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+      this.sortBy === 'name'
+        ? this.visibleSessions.sort(sortByNameAsc)
+        : this.visibleSessions.sort(sortByVotesDesc);
+    }
   }
 
+  public filterSessions(filter: string): void {
+    if (filter === 'all') {
+      //cloning the sessions array by creating a duplicate
+      this.visibleSessions = this.sessions.slice(0);
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter;
+      });
+    }
+  }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession): number {
+  if (s1.name > s2.name) {
+    return 1;
+  } else if (s1.name === s2.name) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession): number {
+  return s2.voters.length - s1.voters.length;
 }
